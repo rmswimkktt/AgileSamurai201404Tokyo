7eefb223df0a178407a822cf3e3c293f45eaf1d5
D.Maruta <rmswimkktt@gmail.com>
2014-04-20 14:49:25 +0900

    3の実装

---
+++ src/FizzBuzz.java
@@ -0,0 +1,14 @@
+
+public class FizzBuzz {
+
+	/**
+	 * @param args
+	 */
+	public String convert(int src){
+		if(src % 3 == 0){
+			return "Fizz";
+		}
+		return String.valueOf(src);
+	}
+
+}
---
+++ src/FizzBuzzTest.java
@@ -0,0 +1,24 @@
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FizzBuzzTest {
+
+	@Test
+	public void _1を渡したら1を返すべき() {
+		FizzBuzz sut = new FizzBuzz();
+		assertThat(sut.convert(1), is("1"));
+	}
+	@Test
+	public void _2を渡したら2を返すべき() {
+		FizzBuzz sut = new FizzBuzz();
+		assertThat(sut.convert(2), is("2"));
+	}
+	@Test
+	public void _3を渡したらFizzを返すべき() {
+		FizzBuzz sut = new FizzBuzz();
+		assertThat(sut.convert(3), is("Fizz"));
+	}
+
+}
